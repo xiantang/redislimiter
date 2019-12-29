b228ca4821953a621091d512639f80fd886b0c34
xiantang <zhujingdi1998@gmail.com>
2019-12-29 18:45:15 +0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "redislimiter"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+libraryDependencies += "com.github.etaty" %% "rediscala" % "1.8.0"
---
+++ src/main/scala/RedisPermits.scala
@@ -0,0 +1,14 @@
+/**
+ * @author zhujingdi
+ * @since 2019/12/29
+ */
+class RedisPermits(
+                    maxPermits: Long,
+                    storePermits: Long,
+                    intervalMillis: Long,
+                    nextFreeTicketMillis: Long
+                  ) {
+
+
+
+}
